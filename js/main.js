fe969b9c88a905c8383b36e5a942f527f30467af
malte9202 <malte.niedereichholz@gmail.com>
2021-01-16 11:28:26 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -94,7 +94,6 @@ const addClickListenerToCheckbox = (checkbox) => {
     checkbox.addEventListener(("click"), (event) => {
         toDoList.removeItemFromList(checkbox.id);
         updatePersistentData(toDoList.getList());
-        refreshThePage();
         const removedText = getLabelText(checkbox.id);
         updateScreenReaderConfirmation(removedText, "removed from list");
         setTimeout(() => {
